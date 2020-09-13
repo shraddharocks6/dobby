f0a4541077e7315c05acf5d744915a0b6ffbac9d
shraddharocks6 <shraddha6pandey@gmail.com>
2020-09-13 17:21:19 +0530

    Trying out stuff

--- contentscript.js
+++ contentscript.js
@@ -12,23 +12,9 @@ document.addEventListener(
   function (e) {
     console.log("Down");
     var selection = window.getSelection().toString();
-    fetch(
-      `https://en.wikipedia.org/w/api.php?action=query&titles=${selection}&origin=*&prop=extracts&format=json`
-    )
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (response) {
-        var endd = response.query.pages;
-
-        data = endd[Object.keys(endd)[0]].extract;
-        if (selection.length > 0) {
-          renderBubble(e.clientX, e.clientY, selection);
-        }
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+    if (selection.length > 0) {
+      renderBubble(e.clientX, e.clientY, selection);
+    }
   },
   false
 );
@@ -45,15 +31,18 @@ document.addEventListener(
 
 // Move that bubble to the appropriate location.
 function renderBubble(mouseX, mouseY, selection) {
-  console.log(selection);
-  const html = `<iframe
-  src="https://en.m.wikipedia.org/wiki/${selection}"
-  title="W3Schools Free Online Web Tutorials"
-  height="600"
-  width="100%"
-    ></iframe>`;
+  console.log("Selection : " + selection);
+  let html = `Hello`;
   bubbleDOM.innerHTML = html;
   bubbleDOM.style.top = mouseY + "px";
   bubbleDOM.style.left = mouseX + "px";
   bubbleDOM.style.visibility = "visible";
+}
+{
+  /* <iframe
+  src="https://en.m.wikipedia.org/wiki/${selection}" 
+  title="W3Schools Free Online Web Tutorials" 
+  height="600" 
+  width="100%"
+    ></iframe> */
 }
